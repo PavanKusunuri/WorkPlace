5a7f1900fe0a7bcd8daef566ffaab608525b43e8
Pavan <pavantejakusunuri@gmail.com>
2020-05-17 06:35:30 +0530

    Added Login Component in Application

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,13 +11,15 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = (e) => setFormData({ ...formData, [name]: e.target.value });
-  const onSubmit = (e) => {
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords donot Match");
     } else {
-      console.log(formData);
+      console.log("SUCCESS");
     }
   };
   return (
@@ -72,7 +75,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
