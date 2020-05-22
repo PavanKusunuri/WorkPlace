669318aa443e2f223faf22f440ad02342708be9b
Pavan <pavantejakusunuri@gmail.com>
2020-05-22 05:30:56 +0530

    Create Profile Component is created

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import createProfile from "./components/profile/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -13,6 +14,7 @@ import store from "./store";
 import "./App.css";
 import setAuthToken from "./utils/setAuthToken";
 import { loadUser } from "./actions/auth";
+import CreateProfile from "./components/profile/CreateProfile";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,6 +36,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
