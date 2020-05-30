db23ddc42c5aafa751fd453e14ad18a4e86635ce
Pavan <pavantejakusunuri@gmail.com>
2020-05-30 14:44:29 +0530

    Adding Post functionality in Application

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AddExperience from "./components/profile-froms/AddExperience";
 import AddEducation from "./components/profile-froms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -63,6 +65,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
