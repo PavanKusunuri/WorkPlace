77707158de4c316cc8ea0f472330b35b53da5120
Pavan <pavantejakusunuri@gmail.com>
2020-12-07 21:23:48 +0530

    Configuring eslint in project

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -35,6 +35,8 @@ export const getProfiles = () => async (dispatch) => {
   try {
     const res = await axios.get(`${backendUrl}/api/profile`);
 
+
+
     dispatch({
       type: GET_PROFILES,
       payload: res.data,
