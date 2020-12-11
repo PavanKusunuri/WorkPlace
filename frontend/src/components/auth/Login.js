20b498df44dbbf10d8131afe9e21c85861a720b2
Pavan <pavantejakusunuri@gmail.com>
2020-12-11 23:03:13 +0530

    Updated Login Component

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -7,10 +7,8 @@ import PropTypes from "prop-types";
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
@@ -29,11 +27,8 @@ const useStyles = makeStyles({
   },
 });
 
-
-
 const Login = ({ login, isAuthenticated }) => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
   const [formData, setFormData] = useState({
     email: "",
     password: "",
