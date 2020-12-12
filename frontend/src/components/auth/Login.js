54942ecafa50b31406ac29cc74a00774a76c77aa
Pavan <pavantejakusunuri@gmail.com>
2020-12-12 19:04:02 +0530

    Added React bootstrap in project

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -4,84 +4,17 @@ import { connect } from "react-redux";
 import { login } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-
-// import { makeStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-// import TextField from '@material-ui/core/TextField';
-// import FormControl from '@material-ui/core/FormControl';
-// import OutlinedInput from '@material-ui/core/OutlinedInput';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import clsx from 'clsx';
-// import { makeStyles } from '@material-ui/core/styles';
-// import IconButton from '@material-ui/core/IconButton';
-// import Input from '@material-ui/core/Input';
-// import FilledInput from '@material-ui/core/FilledInput';
-// import OutlinedInput from '@material-ui/core/OutlinedInput';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import InputAdornment from '@material-ui/core/InputAdornment';
-// import FormHelperText from '@material-ui/core/FormHelperText';
-// import FormControl from '@material-ui/core/FormControl';
-// import TextField from '@material-ui/core/TextField';
-// import Visibility from '@material-ui/icons/Visibility';
-// import VisibilityOff from '@material-ui/icons/VisibilityOff';
-
-// const useStyles = makeStyles({
-//   root: {
-//     maxWidth: 600,
-//     '& .MuiTextField-root': {
-//       margin: '20px',
-//       width: '25ch',
-//     },
-//   },
-//   bullet: {
-//     display: 'inline-block',
-//     margin: '0 2px',
-//     transform: 'scale(0.8)',
-//   },
-//   title: {
-//     fontSize: 14,
-//   },
-//   pos: {
-//     marginBottom: 12,
-//   },
-//   margin: {
-//     margin: 20,
-//   },
-//   aligncenter: {
-//     display: 'flex',
-//     justifyContent: 'center'
-//   }
-// });
-
 const Login = ({ login, isAuthenticated }) => {
-  // const classes = useStyles();
-  const [values, setValues] = React.useState({
+  const [values, setValues] = useState({
     email: '',
     password: '',
   });
-  // const [formData, setFormData] = useState({
-  //   email: "",
-  //   password: "",
-  // });
 
   const { email, password } = values;
 
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
-
-  // const handleClickShowPassword = () => {
-  //   setValues({ ...values, showPassword: !values.showPassword });
-  // };
-
-  // const handleMouseDownPassword = (event) => {
-  //   event.preventDefault();
-  // };
-
-  // const onChange = (e) =>
-  //   setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -94,23 +27,9 @@ const Login = ({ login, isAuthenticated }) => {
   }
   return (
     <Fragment>
-      {/* <div className={classes.aligncenter}> */}
-
       <h1 className="medium">Login</h1>
-
       <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
-          {/* <FormControl fullWidth={true} className={clsx(classes.margin, classes.textField)} variant="outlined">
-
-              <TextField
-                id="email"
-                label="Email"
-                name="email"
-                value={email}
-                onChange={(e) => handleChange(e)}
-                variant="outlined"
-              />
-            </FormControl> */}
           <input
             type="email"
             placeholder="Email Address"
@@ -119,28 +38,6 @@ const Login = ({ login, isAuthenticated }) => {
             onChange={(e) => handleChange(e)}
           />
         </div>
-        {/* <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={values.showPassword ? 'text' : 'password'}
-              value={values.password}
-              onChange={handleChange('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={70}
-            />
-          </FormControl> */}
         <input
           type="password"
           placeholder="Password"
@@ -150,13 +47,7 @@ const Login = ({ login, isAuthenticated }) => {
           onChange={(e) => handleChange(e)}
         />
       </form>
-      {/* </CardContent> */}
-      {/* <Button variant="outlined" onClick={onSubmit} size="medium" color="primary" className={classes.margin}>
-        Login
-        </Button> */}
       <p className="my-1">Don't have an account? <Link to="/register">Register</Link></p>
-      {/* </Card> */}
-      {/* </div > */}
     </Fragment >
   );
 };
