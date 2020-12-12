ed24843a3f59d78666ecda15306ae261f4f8e1b8
Pavan <pavantejakusunuri@gmail.com>
2020-12-12 11:20:03 +0530

    Added Material Icons in Code

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -9,10 +9,31 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import clsx from 'clsx';
+// import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+// import OutlinedInput from '@material-ui/core/OutlinedInput';
+// import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+// import FormControl from '@material-ui/core/FormControl';
+// import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    '& .MuiTextField-root': {
+      margin: '20px',
+      width: '25ch',
+    },
   },
   bullet: {
     display: 'inline-block',
@@ -32,15 +53,32 @@ const useStyles = makeStyles({
 
 const Login = ({ login, isAuthenticated }) => {
   const classes = useStyles();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+  const [values, setValues] = React.useState({
+    email: '',
+    password: '',
+    showPassword: false,
   });
+  // const [formData, setFormData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
 
-  const { email, password } = formData;
+  const { email, password } = values;
 
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  // const onChange = (e) =>
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -58,23 +96,54 @@ const Login = ({ login, isAuthenticated }) => {
         <CardContent>
           <form className="form" onSubmit={(e) => onSubmit(e)}>
             <div className="form-group">
-              <input
+              {/* import TextField from '@material-ui/core/TextField'; */}
+              <TextField
+                id="email"
+                label="Email"
+                name="email"
+                value={email}
+                onChange={(e) => handleChange(e)}
+                variant="outlined"
+              />
+              {/* <input
                 type="email"
                 placeholder="Email Address"
                 name="email"
                 value={email}
                 onChange={(e) => onChange(e)}
-              />
+              /> */}
             </div>
             <div className="form-group">
-              <input
+              <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  labelWidth={70}
+                />
+              </FormControl>
+              {/* <input
                 type="password"
                 placeholder="Password"
                 name="password"
                 minLength="6"
                 value={password}
                 onChange={(e) => onChange(e)}
-              />
+              /> */}
             </div>
           </form>
         </CardContent>
