376c3e8e6ee7091fd7710e96acc240170d5fb8ed
Pavan <pavantejakusunuri@gmail.com>
2021-01-02 23:23:46 +0530

    Import react-router-bootstrap component and modified header component in code

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from "prop-types";
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { logout } from "../../actions/auth";
-const Navbar = () => {
+
+const Header = () => {
 
   const dispatch = useDispatch();
   const userLogin = useSelector(state => state.userLogin)
@@ -12,45 +13,111 @@ const Navbar = () => {
   const logoutHandler = () => {
     dispatch(logout())
   }
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to="/profiles">Developers</Link>
-      </li>
-      <li>
-        <Link to="/employers">For Employers</Link>
-      </li>
-      <li>
-        <Link to="/posts">Posts</Link>
-      </li>
-      <li>
-        <Link to="/home">
-          <span className="hide-sm">Home</span>
-        </Link>
-      </li>
-      <li>
-        <a onClick={logout} href="#!">
-          <i className="fas fa-signout-alt"> </i>
-          {""}
-          <span className="hide-sm">logout</span>
-        </a>
-      </li>
-    </ul>
-  );
+  //   const authLinks = (
+  //     <ul>
+  //       <li>
+  //         <Link to="/profiles">Developers</Link>
+  //       </li>
+  //       <li>
+  //         <Link to="/employers">For Employers</Link>
+  //       </li>
+  //       <li>
+  //         <Link to="/posts">Posts</Link>
+  //       </li>
+  //       <li>
+  //         <Link to="/home">
+  //           <span className="hide-sm">Home</span>
+  //         </Link>
+  //       </li>
+  //       <li>
+  //         <a onClick={logout} href="#!">
+  //           <i className="fas fa-signout-alt"> </i>
+  //           {""}
+  //           <span className="hide-sm">logout</span>
+  //         </a>
+  //       </li>
+  //     </ul>
+  //   );
 
-  const guestLinks = (
-    <ul>
-      <li>
-        <Link to="/profiles">Developers</Link>
-      </li>
-      <li>
-        <Link to="/register">Register</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-    </ul>
-  );
-};
+  //   const guestLinks = (
+  //     <ul>
+  //       <li>
+  //         <Link to="/profiles">Developers</Link>
+  //       </li>
+  //       <li>
+  //         <Link to="/register">Register</Link>
+  //       </li>
+  //       <li>
+  //         <Link to="/login">Login</Link>
+  //       </li>
+  //     </ul>
+  //   );
+  // };
 
-export default Navbar;
+  return (
+    <header>
+      <Navbar bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Work Place</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Collapse id="basic-navbar-nav">
+            {userInfo ? (
+              <>
+                <NavDropdown title={userInfo.name}
+                  id='username'>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      Developers
+                </NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      For Employers
+                </NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      Posts
+                </NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/home">
+                    <NavDropdown.Item>
+                      Home
+                </NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </>
+            ) : (
+                <>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      Developers
+                </NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      For Employers
+                </NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/profiles">
+                    <NavDropdown.Item>
+                      Posts
+                </NavDropdown.Item>
+                  </LinkContainer>
+                </>
+              )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header;
