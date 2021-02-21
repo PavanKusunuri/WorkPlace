c7ace5cb741e57ce9e92f865f1229ad0286588a1
PavanKusunuri <pavantejakusunuri@gmail.com>
2021-02-21 20:22:39 +0530

    Added Company Model in code

--- backend/models/companyModel.js
+++ backend/models/companyModel.js
@@ -1,3 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-const schema = mongoose.schema
+const schema = mongoose.schema;
+
+const CompanySchema = new Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+    websiteUrl: {
+        type: String
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    companyLevel: {
+        type: String,
+        required: String
+    },
+    createdAt: {
+        type: Date,
+        defaultDate: Date.now,
+    }
+});
+
+const Company = mongoose.model('Company', CompanySchema)
+
+export default Company;
