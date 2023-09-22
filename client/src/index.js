aa1fe99758140ff4bcb55c451b51acb7f82c05cb
Pavan Kusunuri <pavantejakusunuri@gmail.com>
2023-09-22 22:29:00 +0530

    CSS changes for better UI

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app =  document.getElementById('root');
+
+// Create a root
+const root = createRoot(app);
+
+// render app to root
+root.render(<App/>)
